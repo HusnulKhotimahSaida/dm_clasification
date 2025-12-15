b0567e154c98a82ec9b7d3d1f5ebc8a81f466610
HusnulKhotimahSaida <husnulkhotimahsaida@gmail.com>
2025-12-15 17:04:39 +0800

    Modify Streamlit app to read and display CSV
    Updated Streamlit app to display CSV file contents.

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import streamlit as st
+import pandas as pd
+import os
 
-st.set_page_config(page_title="Test App")
-st.title("✅ STREAMLIT HIDUP")
-st.write("Kalau ini muncul, deployment sudah benar.")
+st.title("✅ STREAMLIT + CSV TEST")
+
+st.write("📁 File di repository:")
+st.write(os.listdir())
+
+df = pd.read_csv("synthetic_customers_cleaned (1).csv")
+st.success("CSV berhasil dibaca!")
+st.write(df.head())
